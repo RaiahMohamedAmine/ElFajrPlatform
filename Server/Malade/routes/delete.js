eb559e7d746bab2dfa162863f9a5d02ddaa7f6ad
Raiah Mohamed Amine <hm_raiah@esi.dz>
2020-01-16 14:17:26 +0100

    DEV : Delete Malade Added

---
+++ Server/Malade/routes/delete.js
@@ -0,0 +1,18 @@
+var Router = require('express').Router()
+var Model = require ('../DBModel')
+
+function Delete (req, res) {
+    Model.deleteOne ({id : req.body.id}, (err)=>{
+        if (err) 
+            res.json({type: "Err", message : "Malade non trouve"})
+        else   
+            res.json({type:"Info", message : "Malade supprime"})
+    })
+}
+
+Router.post('/delete/:id', Delete)
+
+module.exports ={
+    Router,
+    Delete
+}
