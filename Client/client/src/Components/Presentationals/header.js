24f0ba5c9bead84aa34970de154e7739d01ff38b
Raiah Mohamed Amine <hm_raiah@esi.dz>
2020-02-28 18:24:56 +0100

    fix : changing some paths

--- Client/client/src/Components/Presentationals/header.js
+++ Client/client/src/Components/Presentationals/header.js
@@ -7,7 +7,7 @@ const Header = ({
     return <div className='header'>
         <div className='header-left'>
             <span className='header-logo'></span>
-            <h2 className='header-title'>Association El Fajr</h2>
+            <h2 className='header-title'>Association El Fadjr</h2>
         </div>
         <div className='header-settings' onClick={e => onClick()}>
         </div>
