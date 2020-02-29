154c5d4b8c489fa29fff579672dc0f9e804f5001
Sehdi Nassim <hm_sehdi@esi.dz>
2020-02-29 17:27:13 +0100

    local changes

--- Client/client/src/Components/Containers/Search.js
+++ Client/client/src/Components/Containers/Search.js
@@ -2,22 +2,23 @@ import { connect } from 'react-redux';
 import Search from './../Presentationals/search';
 import setMalades from './../../ReduxStuff/ActionsCreators/setMalades';
 import fetchMalades from './../../ReduxStuff/ActionsCreators/fetchMalades';
+import receiveMalades from './../../ReduxStuff/ActionsCreators/receiveMalades';
 
-const mapDispatchToProps= dispatch =>({
-    onClick(data){
-        dispatch(
-          fetchMalades(data)
-        )
-    },
-    onChange(data){
-      data==="" ? 
-      dispatch (setMalades ([])) :
+const mapDispatchToProps = dispatch => ({
+  onClick(data) {
+    dispatch(
+      fetchMalades(data)
+    )
+  },
+  onChange(data) {
+    data === "" ?
+      dispatch(setMalades([])) :
       dispatch(
         fetchMalades(data)
       )
   }
 })
 
-const MySearch = connect(null,mapDispatchToProps)(Search)
+const MySearch = connect(null, mapDispatchToProps)(Search)
 
 export default MySearch
