b868e6ff3e28a02964216e513d0e927a897dd008
Sehdi Nassim <hm_sehdi@esi.dz>
2020-04-17 20:00:16 +0100

    Completed the stats page

--- Client/client/src/Components/Presentationals/Form-Items/date-input.js
+++ Client/client/src/Components/Presentationals/Form-Items/date-input.js
@@ -7,20 +7,12 @@ const DateInput=({
     onChange,
     modify,
     value,
-    rdv= false,
-    required
+    min,
+    max,
 })=>{
     return <div className='date-input'>
         <label className='date-input-title'>{title}</label>
-        <input className='date-input-input' value={value} type='date' name= {name} min={rdv ? getInputDate () : null} max={rdv? null: getInputDate(false)} onChange={onChange} required disabled={modify}/>
+        <input className='date-input-input' value={value} type='date' name= {name} min={min} max={max} onChange={onChange} required disabled={modify}/>
     </div>
-}
-function getInputDate(tomorrow=true)
-{
-    var day = new Date ();
-    tomorrow ? day.setDate (day.getDate()+1): tomorrow =false;
-    const date = day.getDate() < 10 ? "0" + day.getDate() : day.getDate() ;
-    const month = day.getMonth() < 9 ? "0" + (day.getMonth() + 1) : (day.getMonth() + 1);
-    return day.getFullYear() + "-" + month + "-" + date
 }
 export default DateInput
